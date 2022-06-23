218479bee8641622fd2de2d222856d8b367dab28
brenda200 <brendadiniz2612sz@gmail.com>
2022-06-23 11:36:44 -0300

    primeiro commit

---
+++ assets/js/summernote2.js
@@ -0,0 +1,4 @@
+(function (e) {
+    'use strict';
+    $('#summernote2').summernote();
+})();
---
+++ confie.php
@@ -0,0 +1,7 @@
+<?php
+$local = "localhost";
+$user = "root";
+$senha = "";
+$banco = "desafio";
+$conecta    = mysqli_connect($local,$user,$senha,$banco);
+?>
